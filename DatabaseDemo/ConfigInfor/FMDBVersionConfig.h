99be6f02d1042cbad6d989860f31b45e96774d01
miao <miao@zhaomiaomiao-3.local>
2019-05-21 19:41:16 +0800

    Create FMDBVersionConfig.h

---
+++ DatabaseDemo/ConfigInfor/FMDBVersionConfig.h
@@ -0,0 +1,11 @@
+//
+//  FMDBVersionConfig.h
+//  DatabaseDemo
+//
+//  Created by miao on 2019/5/15.
+//  Copyright © 2019年 miao. All rights reserved.
+//
+
+
+//当前版本
+#define FMDBReleaseCurrentVersion                                               @"2.0"
