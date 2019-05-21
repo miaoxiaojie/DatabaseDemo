5fce6d42efbe5a0ccfea33ef6dc0b775b53b6113
miao <miao@zhaomiaomiao-3.local>
2019-05-21 19:42:11 +0800

    Create FMDBCityDBHelper.h

---
+++ DatabaseDemo/FunctionBuiness/Home/Models/DBHelper/FMDBCityDBHelper.h
@@ -0,0 +1,18 @@
+//
+//  FMDBCityDBHelper.h
+//  DatabaseDemo
+//
+//  Created by miao on 2019/5/16.
+//  Copyright © 2019年 miao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FMDBGoodsDBHelperInterface.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FMDBCityDBHelper : NSObject<FMDBGoodsDBHelperInterface>
+
+@end
+
+NS_ASSUME_NONNULL_END
