234f6c327935e3808568c982581942a2d2a35c2b
miao <miao@zhaomiaomiao-3.local>
2019-05-21 19:41:28 +0800

    Create FMDBAppUpgradeDBHelper.h

---
+++ DatabaseDemo/FunctionBuiness/AppUpgrade/scr/DBHelper/FMDBAppUpgradeDBHelper.h
@@ -0,0 +1,18 @@
+//
+//  FMDBAppUpgradeDBHelper.h
+//  DatabaseDemo
+//
+//  Created by miao on 2019/5/15.
+//  Copyright © 2019年 miao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FMDBAppUpgradeDBHelperInterface.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FMDBAppUpgradeDBHelper : NSObject<FMDBAppUpgradeDBHelperInterface>
+
+@end
+
+NS_ASSUME_NONNULL_END
