6bef464afeeb0eb33f099e44864ffd9145d5ee3c
miao <miao@zhaomiaomiao-3.local>
2019-05-21 19:41:39 +0800

    Create FMDBAppUpgradeManager.h

---
+++ DatabaseDemo/FunctionBuiness/AppUpgrade/scr/Manager/FMDBAppUpgradeManager.h
@@ -0,0 +1,18 @@
+//
+//  FMDBAppUpgradeManager.h
+//  DatabaseDemo
+//
+//  Created by miao on 2019/5/15.
+//  Copyright © 2019年 miao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FMDBAppUpgradeManagerInterface.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FMDBAppUpgradeManager : NSObject<FMDBAppUpgradeManagerInterface>
+
+@end
+
+NS_ASSUME_NONNULL_END
