941955c75cbae00a4a57aec1836494d99d6ade22
miao <miao@zhaomiaomiao-3.local>
2019-05-21 19:40:28 +0800

    Create AppDelegate+FMDBFinishLaunch.h

---
+++ DatabaseDemo/AppDelegate/AppDelegate+FMDBFinishLaunch.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate+FMDBFinishLaunch.h
+//  DatabaseDemo
+//
+//  Created by miao on 2019/5/17.
+//  Copyright © 2019年 miao. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppDelegate (FMDBFinishLaunch)
+
+@end
+
+NS_ASSUME_NONNULL_END
