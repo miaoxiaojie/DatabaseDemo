af306c5ea2eafbc3b768cdae3b36889fbfae0452
miao <miao@zhaomiaomiao-3.local>
2019-05-21 19:42:48 +0800

    Create FMDBDBModule.h

---
+++ DatabaseDemo/Modules/LaunchModules/FMDBDBModule.h
@@ -0,0 +1,17 @@
+//
+//  FMDBDBModule.h
+//  DatabaseDemo
+//
+//  Created by miao on 2019/5/14.
+//  Copyright © 2019年 miao. All rights reserved.
+//
+
+#import "FMDBStartBaseModule.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FMDBDBModule : FMDBStartBaseModule
+
+@end
+
+NS_ASSUME_NONNULL_END
